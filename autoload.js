7e0cfdaa6bbbc10795932cc4d1cb462da0d6dbc7
DanceDream <111162406+DanceDreamIO@users.noreply.github.com>
2023-07-26 17:20:22 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -36,7 +36,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://net.dancedream.top/live2d/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
